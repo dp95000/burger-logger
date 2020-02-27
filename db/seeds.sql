70490c1479ee7b53433da0a9689e2edc6c8d1c63
dp <davepollan@gmail.com>
2020-02-27 01:46:59 -0500

    front end application ready

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
+var express = require("express");
+var PORT = process.env.PORT || 3000;
+var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello Precious World!'));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
